5e618e338d39c23097b89eff470e01ca0f7a00cb
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-19 07:22:16 +0000

    Adding tarfile member sanitization to extractall()

--- loader_voxforge.py
+++ loader_voxforge.py
@@ -315,7 +315,26 @@ class VOXFORGE(data.Dataset):
                     full_path = os.path.join(self.langzipsdir, fp)
                     # extract zip file
                     with tarfile.open(full_path) as zip_f:
-                        zip_f.extractall(self.langzipsdir)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(zip_f, self.langzipsdir)
                 # copy prompts
                 promptdir = os.path.join(tardir, "etc")
                 if os.path.isfile(os.path.join(promptdir, "prompts-original")):
